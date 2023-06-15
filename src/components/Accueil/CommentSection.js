2a0b4ae6944dda3841f1db6a8d7d323e047d1f3e
Valmir Llapashtica <val.66@hotmail.com>
2023-06-15 16:30:41 +0200

    Ticket #12 - Setup CommentSection to display the 3 most recent comment

--- src/components/Accueil/CommentSection.js
+++ src/components/Accueil/CommentSection.js
@@ -5,35 +5,73 @@ import { useState } from 'react';
 export default function CommentSection() {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
+  const [message, setMessage] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+
+    if (message && rating) {
+      const newComment = {
+        message: message,
+        rating: rating
+      };
+      setComments([newComment, ...comments.slice(0, 2)]);
+      setMessage('');
+      setRating(null);
+    };
+  };
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  }
   return (
     <Comments>
-      <TitleComments>Donnez votre avis sur nos service:</TitleComments>
-      <Message rows="10"></Message>
-      <div>
-        {[...Array(5)].map((start, index) => {
-          const currentRating = index + 1;
-          return (
-            <label>
-              <input
-                type="radio"
-                name="rating"
-                value={currentRating}
-                onClick={() => setRating(currentRating)}
-              />
-              <FaStar
-                className="star"
-                size={25}
-                color={currentRating <= (hover || rating) ? "#242425" : "e4e5e9"}
-                onMouseEnter={() => setHover(currentRating)}
-                onMouseLeave={() => setHover(null)}
-              />
-            </label>
-          )
-        })}
-      </div>
-      <Send>Envoyer</Send>
+      <Form onSubmit={handleCommentSubmit}>
+        <TitleComments>Donnez votre avis sur nos service:</TitleComments>
+        <Message
+          rows="10"
+          value={message}
+          onChange={handleChange}
+          placeholder="Entrer votre commentaire"></Message>
+        <div>
+          {[...Array(5)].map((start, index) => {
+            const currentRating = index + 1;
+            return (
+              <label key={index}>
+                <input
+                  type="radio"
+                  name="rating"
+                  value={currentRating}
+                  onClick={() => setRating(currentRating)}
+                />
+                <FaStar
+                  className="star"
+                  size={25}
+                  color={currentRating <= (hover || rating) ? "#242425" : "e4e5e9"}
+                  onMouseEnter={() => setHover(currentRating)}
+                  onMouseLeave={() => setHover(null)}
+                />
+              </label>
+            )
+          })}
+        </div>
+        <Send type="submit">Envoyer</Send>
+      </Form>
+
       <TitleComments>Avis récent:</TitleComments>
-      <p>Note {rating}/5</p>
+      {comments.length > 0 ? (
+        <ul>
+          {comments.slice(0, 3).map((comment, index) => (
+            <li key={index}>
+              <p>{comment.message}. {comment.rating}/5 <FaStar /></p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Soyer le premier commentaire.</p>
+      )
+      }
     </Comments>
   );
 };
@@ -43,6 +81,7 @@ const Comments = styled.div`
 display: flex;
 flex-direction: column;
 margin: 20px 0 60px 0;
+padding: 10px;
 background-color: #F5CB5C;
 width: 100%;
 height: 500px;
@@ -53,6 +92,11 @@ height: 500px;
   cursor: pointer;
 }
 `;
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+`;
+
 
 const TitleComments = styled.label`
 margin-top: 15px;
