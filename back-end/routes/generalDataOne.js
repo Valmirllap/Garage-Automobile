09b4af332f6bcee5c8e594751d715caee30a1eee
Valmir Llapashtica <val.66@hotmail.com>
2023-10-19 17:43:12 +0200

    Ticket #42 - fix bug

--- back-end/routes/generalDataOne.js
+++ back-end/routes/generalDataOne.js
@@ -1,13 +1,13 @@
 const { generalDataOneCreate ,generalDataOneGet, generalDataOneUpdate, generalDataOneDelete } = require("../controllers/generalDataOneController");
 
 function DataOneCarRoute(app) {
-// ============ ROUTE: CREATE GENERAL DATA ONE ============
+// ============ ROUTE: CREATE GENERAL DATA ONE ========================
 app.post("/dataone/create/:car_id", generalDataOneCreate);
-// ============ ROUTE: READ GENERAL DATA ONE ============
+// ============ ROUTE: READ GENERAL DATA ONE ========================
 app.get("/dataone/get/:car_id", generalDataOneGet);
-// ============ ROUTE: UPDATE GENERAL DATA ONE ============
+// ============ ROUTE: UPDATE GENERAL DATA ONE ========================
 app.put("/dataone/update/:id", generalDataOneUpdate);
-// ============ ROUTE: DELETE GENERAL DATA ONE ============
+// ============ ROUTE: DELETE GENERAL DATA ONE ========================
 app.delete("/dataone/delete/:id", generalDataOneDelete);
 }
 
